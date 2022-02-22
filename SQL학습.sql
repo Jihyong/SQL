e87b5474193322778204f609b43109801ba01833
이지형 <jihyong94@naver.com>
2022-02-22 22:46:52 +0900

    Day1~Day4 학습

--- SQL학습.sql
+++ SQL학습.sql
@@ -229,3 +229,236 @@ SELECT  SUM(SALARY), -- SELECT절에 SUM같은 그룹함수가 사용되면 일반 속성은 쓸수 
         MAX(HIRE_DATE),
         MIN(HIRE_DATE)
 FROM    EMPLOYEE;
+
+--DAY03
+-- ORDER BY
+SELECT *
+FROM    EMPLOYEE;
+
+SELECT *
+FROM    EMPLOYEE
+ORDER BY    SALARY DESC , EMP_NAME DESC; -- 오름차순[ASC]이 기본 디폴트, [DESC] 내림차순
+
+-- 별칭을 기준으로 ORDER BY 가능
+SELECT  EMP_NAME AS 이름,
+        HIRE_DATE AS 입사일,
+        DEPT_ID AS 부서코드
+FROM    EMPLOYEE
+ORDER BY    부서코드 DESC, 입사일, 이름;  .
+
+-- 인덱스 사용 가능
+SELECT  EMP_NAME AS 이름, --인덱스 1
+        HIRE_DATE AS 입사일,--인덱스 2
+        DEPT_ID AS 부서코드 --인덱스 3
+FROM    EMPLOYEE
+ORDER BY    3 DESC, 2, 1; -- ORDER BY 인덱스
+
+
+-- GROUP BY: 하위 데이터 그룹
+SELECT  DEPARTMENT_NO 학과번호,
+        COUNT(*) "학생수(명)"
+FROM    TB_STUDENT
+GROUP BY    DEPARTMENT_NO
+ORDER BY    1 ASC;
+
+-- 부서별 평균 급여를 조회한다면?
+SELECT  DEPT_ID 부서,
+        FLOOR(AVG(SALARY)) 평균급여
+FROM    EMPLOYEE
+GROUP BY    DEPT_ID
+
+-- 성별에 따른 평균 급여 조회
+SELECT  CASE WHEN SUBSTR(EMP_NO,8,1) IN ('1','3') THEN 'MALE' ELSE 'FEMALE' END AS 성별,
+        FLOOR(AVG(SALARY)) 평균급여
+FROM    EMPLOYEE
+GROUP BY    SUBSTR(EMP_NO,8,1);--CASE WHEN SUBSTR(EMP_NO,8,1) IN ('1','3') THEN 'MALE' ELSE 'FEMALE' END 표현식이 들어가는 게 더 정확
+
+SELECT  DEPT_ID,
+        EMP_NAME,
+        COUNT(*)
+FROM    EMPLOYEE
+GROUP BY    DEPT_ID, EMP_NAME;
+
+SELECT  DEPT_ID,
+        SUM(SALARY)
+FROM    EMPLOYEE
+GROUP BY    DEPT_ID
+HAVING  SUM(SALARY) > 9000000; -- GROUP 에 대한 조건은 HAVING, WHERE은 테이블에 대한 조건
+
+--ROLLUP()
+SELECT  DEPT_ID,
+        JOB_ID,
+        SUM(SALARY)
+FROM    EMPLOYEE
+GROUP BY    ROLLUP(DEPT_ID, JOB_ID); -- 소계와 누적 총계출력
+-- ROLLUP( (DEPT_ID, JOB_ID) ) 괄호를 이중으로 씌우면 소계X,누적 총계만 출력
+
+SELECT  DEPT_ID,
+        JOB_ID,
+        SUM(SALARY)
+FROM    EMPLOYEE
+GROUP BY    DEPT_ID, ROLLUP(JOB_ID); -- 소계O, 누적 총계X
+
+-- ERD
+-- EMPLOYEE, DEPARTMENT
+-- PK는 중복을 허용하지 않고, NULL값을 허용하지 않음.
+-- FK는 부모에 의존하는 데이터이거나 ,NULL 값 허용.
+SELECT  *
+FROM    DEPARTMENT;
+
+SELECT  *
+FROM    EMPLOYEE;
+
+-- 사원의 이름, 부서이름 조회
+-- 즉, 두개의 컬럼을 한번에 볼 수 있는 방법
+-- JOIN
+-- EQUALS JOIN 업무적 연관성이 있고, 부모의 
+SELECT  EMP_NAME,
+        DEPT_NAME,
+        E.DEPT_ID
+FROM    EMPLOYEE E,DEPARTMENT D -- 테이블에서도 별칭가능 AS 는 쓰지 않고.
+WHERE   E.DEPT_ID = D.DEPT_ID; -- JOIN을 할 때 모호성이 발생할 수 있기 때문에 테이블에 별칭을 줌
+
+-- ANSI JOIN 표준 조인
+/*
+SELECT
+FROM    TABLE1
+[INNER] JOIN    TABLE2 ON (CONDITION) -- 필요에 따라서 ON 부모의 외래키와 자식의 기본키가 같으면. 관계가 없는 두 테이블에서의 NOT EQUALS JOIN은 ON으로 사용.
+[INNER] JOIN    TABLE2 USING (COLUMN) -- 필요에 따라서 USING,두 개의 테이블이 내부 조인으로 조인 될 때 조인하고자 하는 두 테이블의 컬럼명이 같을 경우 조인 조건을 길게 적지 않고 간단하게 적을 수 있도록 하는 역할
+-- OUTER JOIN: 조건에 만족하지 않는 데이터까지 포함시키는 조인.
+LEFT | RIGHT | FULL [OUTER] JOIN    TABLE2 ON (CONDITION)
+LEFT | RIGHT | FULL [OUTER] JOIN    TABLE2 USING (COLUMN)
+*/
+
+SELECT  EMP_NAME,
+        DEPT_NAME,
+        DEPT_ID,
+        JOB_TITLE,
+        LOC_DESCRIBE
+FROM    EMPLOYEE E
+JOIN    DEPARTMENT D USING(DEPT_ID) --USING을 쓸 때 SELECT 절에 별칭을 쓰지못함
+JOIN    JOB        J  USING(JOB_ID)
+JOIN    LOCATION   L  ON (L.LOCATION_ID = D.LOC_ID); -- LOCATION TABLE에 기본키가 DEPARTMNET에 외래키 LOC_ID와 동일하지 않기에 USING 불가,ON 사용
+
+
+SELECT  EMP_NAME,
+        DEPT_NAME,
+        E.DEPT_ID,
+        J.JOB_TITLE,
+        LOC_DESCRIBE
+FROM    EMPLOYEE E
+JOIN    DEPARTMENT D ON(D.DEPT_ID = E.DEPT_ID)
+JOIN    JOB        J ON(E.JOB_ID = J.JOB_ID)
+JOIN    LOCATION   L  ON (L.LOCATION_ID = D.LOC_ID);
+-- 위에 까지가 ANSI 표준 JOIN
+
+-- 연결고리가 없는 두 테이블 간의 조인
+SELECT  *
+FROM    SAL_GRADE
+
+SELECT  EMP_NAME,
+        SALARY,
+        SLEVEL
+FROM    EMPLOYEE 
+JOIN    SAL_GRADE ON (SALARY BETWEEN LOWEST AND HIGHEST)
+ORDER BY    SLEVEL, SALARY DESC;
+
+--OUTER JOIN 오라클
+SELECT  EMP_NAME,
+        DEPT_NAME
+FROM    EMPLOYEE E, DEPARTMENT D
+WHERE   E.DEPT_ID(+) = D.DEPT_ID -- 오라클 전용: 좌측은 LEFT OUTER JOIN (+) 표시가 있음, RIGHT OUTER JOIN은 오른쪽에 (+), FULL OUTER JOIN은 오라클에서 지원 안함.
+
+--따라서 ANSI 표준으로 LEFT RIGHT FULL OUTER JOIN 
+SELECT  EMP_NAME,
+        DEPT_NAME
+FROM    EMPLOYEE E
+FULL JOIN  DEPARTMENT D USING(DEPT_ID);
+
+-- 사원의 이름과 사수의 이름을 셀프조인으로 조회한다면?, 사수의 사수는?
+SELECT  E.EMP_NAME,
+        M.EMP_NAME,
+        S.EMP_NAME
+FROM    EMPLOYEE E
+JOIN    EMPLOYEE M ON (E.MGR_ID = M.EMP_ID)
+JOIN    EMPLOYEE S ON (E.MGR_ID = S.EMP_ID)
+
+-- LOC_DESCIRBE가 아시아로 시작하고 직급이 대리인 사원의 이름, 부서이름을 조회
+SELECT  LOC_DESCRIBE
+FROM    LOCATION
+
+SELECT  EMP_NAME,
+        DEPT_NAME
+FROM    JOB
+JOIN    EMPLOYEE    USING(JOB_ID)
+JOIN    DEPARTMENT  USING(DEPT_ID)
+JOIN    LOCATION    ON(LOC_ID = LOCATION_ID)
+WHERE   LOC_DESCRIBE LIKE '아시아%' AND JOB_TITLE = '대리';
+
+-- SET 연산자
+/*
+두 개 이상의 쿼리 결과를 하나로 결합시키는 연산자
+- UNION: 합집합, 중복 제거
+- UNION ALL : 합집합 중복 포함
+- INTERSECT : 교집합
+- MINUS : 차집합
+주의) 반드시 동일(컬럼 개수, 데이터 타입)
+*/
+
+SELECT  EMP_ID,
+        ROLE_NAME
+FROM    EMPLOYEE_ROLE;
+UNION ALL
+SELECT  EMP_ID,
+        ROLE_NAME
+FROM    ROLE_HISTORY;
+
+--UNION
+SELECT  TO_CHAR(SALARY),
+        JOB_ID,
+        HIRE_DATE
+FROM    EMPLOYEE
+UNION
+SELECT  DEPT_NAME,
+        DEPT_ID,
+        NULL
+FROM    DEPARTMENT
+WHERE   DEPT_ID = 20;
+
+--UNION 부서번호가 50번 부서의 부서원을 관리자와 직원으로 구분하여 사원번호,이름,구분을 표시
+-- 관리자와 직원을 구분하는 기준은 EMP_ID = '141'이면 관리자
+
+SELECT  EMP_ID,
+        EMP_NAME,
+        '관리자' AS 구분
+FROM    EMPLOYEE
+WHERE   DEPT_ID = '50' AND EMP_ID = '141'
+UNION
+SELECT  EMP_ID,
+        EMP_NAME,
+        '직원' AS 구분
+FROM    EMPLOYEE
+WHERE   DEPT_ID = '50' AND EMP_ID != '141'
+ORDER BY 3;
+
+
+SELECT  EMP_NAME,
+        JOB_TITLE
+FROM    EMPLOYEE
+JOIN    JOB USING(JOB_ID)
+WHERE   JOB_TITLE IN ('대리' , '사원');
+
+
+-- 위 구문을 UNION으로 표현
+SELECT  EMP_NAME,
+        JOB_TITLE
+FROM    EMPLOYEE
+JOIN    JOB USING(JOB_ID)
+WHERE   JOB_TITLE = '대리'
+UNION
+SELECT  EMP_NAME,
+        JOB_TITLE
+FROM    EMPLOYEE
+JOIN    JOB USING(JOB_ID)
+WHERE   JOB_TITLE = '사원'
+ORDER BY 2, 1;
