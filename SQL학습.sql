0fbd16b5d817b51d7a14c53733d39b8993b3fae2
이지형 <jihyong94@naver.com>
2022-02-23 22:21:14 +0900

    day 4

--- SQL학습.sql
+++ SQL학습.sql
@@ -462,3 +462,147 @@ FROM    EMPLOYEE
 JOIN    JOB USING(JOB_ID)
 WHERE   JOB_TITLE = '사원'
 ORDER BY 2, 1;
+--day04
+-- SUBQUERY, DDL(CONSTRAINT 제약)
+
+-- SUBQUERY 서브쿼리(하나의 쿼리가 다른 쿼리를 포함하는 구조)
+/*
+SELECT  EXPR(SELECT) -- SCALLAR SUBQUERY
+FORM    (SELECT) -- INLINE VIEW
+WHERE   (SELECT) -- SUBQUERY, 즉 EXPR OPERATOR (SUBQUERY)
+GROUP BY (SELECT)
+HAVING  (SELECT)
+즉,메인 쿼리 안에 서브쿼리 들어갈 수 있음.
+
+유형
+- 단일 행 서브쿼리 (리턴 되는 레코드 건수가 1개), (단일 컬림, 다중컬럼)
+- 다중 행 서브쿼리 (단일 컬럼, 다중 컬럼)-( IN, ANY, ALL)연산자를 사용.
+*/
+-- '나승원' 직원과 같은 부서원을 조회
+SELECT  *
+FROM    EMPLOYEE
+WHERE   EMP_NAME = '나승원' --여기서 DEPT_ID가 50이라는 것을 확인
+
+
+SELECT  *
+FROM    EMPLOYEE
+WHERE   DEPT_ID = (SELECT  DEPT_ID
+                    FROM    EMPLOYEE
+                    WHERE   EMP_NAME = '나성원');
+
+-- '나승원' 직원과 같은 직급이면서 급여가 '나승원' 보다 많이 받는 직원을 조회한다면?
+SELECT  *
+FROM    EMPLOYEE
+WHERE   JOB_ID = (SELECT    JOB_ID
+                    FROM    EMPLOYEE
+                    WHERE   EMP_NAME = '나승원')
+AND      SALARY > (SELECT    SALARY
+                    FROM    EMPLOYEE
+                    WHERE   EMP_NAME = '나승원')
+
+-- 최저급여를 받는 사원의 정보를 검색
+SELECT  *
+FROM    EMPLOYEE
+WHERE   SALARY = (SELECT    MIN(SALARY)
+                    FROM    EMPLOYEE);
+                    
+-- 부서별 급여총합이 가장 높은 부서의 부서이름, 급여 총합을 조회
+SELECT  DEPT_NAME,
+        SUM(SALARY)
+FROM    EMPLOYEE
+JOIN    DEPARTMENT USING(DEPT_ID)
+GROUP BY DEPT_NAME
+HAVING  SUM(SALARY) = (SELECT   MAX(SUM(SALARY))
+                        FROM    EMPLOYEE
+                        GROUP BY DEPT_ID);
+
+/*
+다중 행 서브쿼리
+> ANY, < ANY
+> ALL, < ALL
+*/
+-- 관리자들만 조회(즉 MGR_ID에 EMP_ID가 있다면 관리자)
+
+SELECT  EMP_ID,
+        EMP_NAME,
+        '관리자' AS "관리자 유무"
+FROM    EMPLOYEE
+WHERE   EMP_ID IN (SELECT   MGR_ID  -- 서브쿼리가 다중 행 이므로 WHERE절에 IN( 다중행 연산자를 씀)
+                    FROM    EMPLOYEE)
+UNION     
+SELECT  EMP_ID,
+        EMP_NAME,
+        '직원' AS "관리자 유무"
+FROM    EMPLOYEE
+WHERE   EMP_ID NOT IN (SELECT   MGR_ID  -- 서브쿼리가 NULL 값이 있으면 NOT IN 을 쓰면 NULL이 리턴됨
+                        FROM    EMPLOYEE
+                        WHERE   MGR_ID IS NOT NULL) --따라서 IS NOT NULL 로 NULL 제거 후 NOT IN 사용
+ORDER BY 3;
+
+-- 대리직급의 사원의 이름, 급여 조회
+-- 과장 직급의 사원의 이름, 급여 조회
+SELECT  EMP_NAME,JOB_TITLE,
+        SALARY
+FROM    EMPLOYEE
+JOIN    JOB USING(JOB_ID)
+WHERE   JOB_TITLE = '대리'
+UNION
+SELECT  EMP_NAME,JOB_TITLE,
+        SALARY
+FROM    EMPLOYEE
+JOIN    JOB USING(JOB_ID)
+WHERE   JOB_TITLE = '과장'
+ORDER BY 2;
+
+-- 과장직급보다 많은 급여를 받는 대리직의 사원이름, 급여를 조회
+SELECT  EMP_NAME,
+        SALARY
+FROM    EMPLOYEE
+JOIN    JOB USING(JOB_ID)
+WHERE   JOB_TITLE = '대리'
+AND  SALARY > ANY (SELECT    SALARY -- < ANY, <ALL > ALL 구분하기, 여기선 >ANY니까 과장급 급여의 최소한도 보다 큰 대리 사원들 출력
+                        FROM    EMPLOYEE
+                        JOIN    JOB USING(JOB_ID)
+                        WHERE   JOB_TITLE = '과장')
+-- 직급별(JOB_TLTLE) 평균 급여를 조회, 정수 5자리에서 절삭
+SELECT  JOB_TITLE, -- 2. 이 부분은 바로 못쓰기에 GROUP BY를 사용
+        TRUNC(AVG(SALARY),-5) -- 1.여기 때문에
+FROM    EMPLOYEE
+JOIN    JOB USING(JOB_ID)
+GROUP BY    JOB_TITLE;
+
+-- 자기 직급의 평균 급여를 받는 직원의 이름, 직급, 급여 조회(다중행 다중 열 서브쿼리)
+SELECT  EMP_NAME,
+        JOB_TITLE,
+        SALARY
+FROM    EMPLOYEE
+JOIN    JOB USING(JOB_ID)
+WHERE   (JOB_TITLE, SALARY) IN (SELECT   JOB_TITLE, TRUNC(AVG(SALARY),-5) --다중 행 다중 열 서브쿼리
+                                FROM    EMPLOYEE
+                                JOIN    JOB USING(JOB_ID)
+                                GROUP BY    JOB_TITLE);
+                    
+-- 위에 문제를 FROM의 서브쿼리로 풀기                   
+SELECT  E.EMP_NAME,
+        JOB_TITLE,
+        V.JOBAVG
+FROM    (SELECT JOB_ID,
+                TRUNC(AVG(SALARY),-5) AS JOBAVG
+            FROM EMPLOYEE
+            JOIN JOB USING(JOB_ID)
+            GROUP BY JOB_ID) V
+JOIN    EMPLOYEE E ON (JOBAVG = SALARY AND E.JOB_ID = V.JOB_ID)
+JOIN    JOB J ON(J.JOB_ID = E.JOB_ID);
+                    
+                    
+-- 상관관계 서브쿼리
+-- 메인쿼리에서 처리되는 각 행의 값에 따라서 서브쿼리의 결과값이 달라지는 경우
+SELECT  EMP_NAME,
+        JOB_TITLE,
+        SALARY
+FROM    EMPLOYEE E
+JOIN    JOB J ON (E.JOB_ID = J.JOB_ID)
+WHERE   SALARY = (SELECT    TRUNC(AVG(SALARY),-5)
+                    FROM    EMPLOYEE
+                    WHERE   JOB_ID = E.JOB_ID); --메인쿼리의 JOB_ID가 서브쿼리로 들어가는 것.
+                    
